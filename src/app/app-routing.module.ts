6340e295b4413c3fc706006747ef1a740cb3fadc
Krishna <krishnabaldwa1101@gmail.com>
2023-02-27 15:28:09 +0530

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminpageComponent } from './adminpage/adminpage.component';
+import { UserpageComponent } from './userpage/userpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'/user', component:UserpageComponent},
+  {path:'/admin', component:AdminpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
